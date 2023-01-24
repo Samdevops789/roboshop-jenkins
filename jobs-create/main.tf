c6699241fb068cdd0de69b9d82db102cc5e02ea0
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-24 23:06:36 +0530

    updating ./learning-devops7/roboshop-jenkins

--- jobs-create/main.tf
+++ jobs-create/main.tf
@@ -1,20 +1,26 @@
-provider  "jenkins" {
-  server_url = "https://172.31.10.206:8080"
-  username   = "admin"
-  password   = "admin123"
-}
-
-terraform {
-  required_providers {
-    jenkins = {
-      source = "registry.terraform.io/taiidani/jenkins"
-    }
-  }
-}
-
 resource "jenkins_folder" "folder" {
   count       = length(var.folders)
   name        = element(var.folders, count.index)
   description = element(var.folders, count.index)
+}
+
+resource "jenkins_job" "infra-jobs" {
+  depends_on = [jenkins_folder.folder]
+  count      = length(var.infra-jobs)
+  name       = element(var.infra-jobs, count.index)
+  folder     = "/job/infrastructure"
+  template = templatefile("${path.module}/pipeline-job.xml", {
+    git_repo = element(var.infra-jobs, count.index)
+  })
+}
+
+resource "jenkins_job" "app-jobs" {
+  depends_on = [jenkins_folder.folder]
+  count      = length(var.app-jobs)
+  name       = element(var.app-jobs, count.index)
+  folder     = "/job/applications"
+  template = templatefile("${path.module}/multi-branch-job.xml", {
+    git_repo = element(var.app-jobs, count.index)
+  })
 }
 
