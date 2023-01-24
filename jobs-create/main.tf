0fd4fdd8bf73c2d509f9805366cb74c11523a67f
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-24 22:21:09 +0530

    updating ./learning-devops7/roboshop-jenkins

---
+++ jobs-create/main.tf
@@ -0,0 +1,13 @@
+provider "jenkins" {
+  server_url = "https://172.31.10.206:8080"
+  username   = "admin"
+  password   = "admin123"
+}
+
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
+}
